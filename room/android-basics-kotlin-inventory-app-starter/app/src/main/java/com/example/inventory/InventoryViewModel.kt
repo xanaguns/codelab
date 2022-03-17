ef714149a71d75195f4820f77b0c71b7180bb3fc
choigunsoo <choi.gunsoo@gmail.com>
2022-03-18 01:40:12 +0900

    [android-basics-kotlin-inventory-app-starter] add InventoryViewModel.kt

---
+++ room/android-basics-kotlin-inventory-app-starter/app/src/main/java/com/example/inventory/InventoryViewModel.kt
@@ -0,0 +1,26 @@
+package com.example.inventory
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.viewModelScope
+import com.example.inventory.data.Item
+import com.example.inventory.data.ItemDao
+import kotlinx.coroutines.launch
+
+class InventoryViewModel(private val itemDao: ItemDao) : ViewModel() {
+    private fun insertItem(item: Item) {
+        viewModelScope.launch {
+            itemDao.insert(item)
+        }
+    }
+}
+
+class InventoryViewModelFactory(private val itemDao: ItemDao) : ViewModelProvider.Factory {
+    override fun <T : ViewModel?> create(modelClass: Class<T>): T {
+        if (modelClass.isAssignableFrom(InventoryViewModel::class.java)) {
+            @Suppress("UNCHECKED_CAST")
+            return InventoryViewModel(itemDao) as T
+        }
+        throw IllegalArgumentException("Unknown ViewModel class")
+    }
+}

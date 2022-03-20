e86e9927cdb16c3e0fc23037143460123d6f8a42
choigunsoo <choi.gunsoo@gmail.com>
2022-03-21 03:24:26 +0900

    [android-basics-kotlin-inventory-app-starter] modified AddItemFragment

--- room/android-basics-kotlin-inventory-app-starter/app/src/main/java/com/example/inventory/InventoryViewModel.kt
+++ room/android-basics-kotlin-inventory-app-starter/app/src/main/java/com/example/inventory/InventoryViewModel.kt
@@ -8,10 +8,24 @@ import com.example.inventory.data.ItemDao
 import kotlinx.coroutines.launch
 
 class InventoryViewModel(private val itemDao: ItemDao) : ViewModel() {
+
+    fun addNewItem(itemName: String, itemPrice: String, itemCount: String) {
+        val newItem = getNewItemEntry(itemName, itemPrice, itemCount)
+        insertItem(newItem)
+    }
+
     private fun insertItem(item: Item) {
         viewModelScope.launch {
             itemDao.insert(item)
         }
+    }
+
+    private fun getNewItemEntry(itemName: String, itemPrice: String, itemCount: String): Item {
+        return Item(
+            itemName = itemName,
+            itemPrice = itemPrice.toDouble(),
+            quantityInStock = itemCount.toInt()
+        )
     }
 }
 

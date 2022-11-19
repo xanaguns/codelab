396b8fe3d400baaa4346163ac8e7bca02cef5692
choigunsoo <choi.gunsoo@gmail.com>
2022-11-19 20:25:25 +0900

    update Unit3_01_NavigateBetweenScreens.kt

--- Android_Basics_in_Kotlin/Unit_1_Kotlin_basics/01_Introduction_to_Kotlin/KotlinIntroductionTest/app/src/main/java/com/guns/kotlinintroduction/Unit3_01_NavigateBetweenScreens.kt
+++ Android_Basics_in_Kotlin/Unit_1_Kotlin_basics/01_Introduction_to_Kotlin/KotlinIntroductionTest/app/src/main/java/com/guns/kotlinintroduction/Unit3_01_NavigateBetweenScreens.kt
@@ -1,6 +1,11 @@
 package com.guns.kotlinintroduction
 
 fun main() {
+    //=====================================================================
+    // 2. Learn about collections
+    //=====================================================================
+
+    // 2.1 Create a list
     val numbers = listOf(0, 3, 8, 4, 0, 5, 5, 8, 9, 2)
     println("list:   ${numbers}")
     println("sorted: ${numbers.sorted()}")
@@ -10,4 +15,28 @@ fun main() {
     val set2 = mutableSetOf(3,2,1)
     println("$set1 == $set2: ${set1 == set2}")
     println("contains 7: ${setOfNumbers.contains(7)}")
+
+    // 2. Learn about maps
+    val peopleAges = mutableMapOf<String, Int>(
+        "Fred" to 30,
+        "Ann" to 23
+    )
+    peopleAges.put("Barbara", 42)
+    peopleAges["Joe"] = 51
+    peopleAges["Fred"] = 31
+    println(peopleAges)
+
+    //=====================================================================
+    // 3. Working with collections
+    //=====================================================================
+
+    // 3.1 forEach
+    peopleAges.forEach { print("${it.key} is ${it.value}, ") }
+
+    // 3.2 map
+    println(peopleAges.map { "${it.key} is ${it.value}" }.joinToString(", ") )
+
+    // 3.3 filter
+    val filteredNames = peopleAges.filter { it.key.length < 4 }
+    println(filteredNames)
 }
